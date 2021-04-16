87fa1294ff23b0bb9200a671df47c78bfb939ef5
arlbibek <aryalbmail@gmail.com>
2021-04-16 22:15:49 +0545

    Added comments

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,8 @@ module.exports = {
     development: {
       network_id: "*",
       host: "localhost",
-      port: 8545,
+      // port: 7545, // for ganache gui
+      port: 8545, // for ganache-cli
       gas: 6721975,
       gasPrice: 20000000000,
     },
